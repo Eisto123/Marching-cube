9e5c6459784d575809b867a87f7c3e912b4f86f9
Eisto123 <lzm505553681@gmail.com>
2025-01-16 15:25:57 +0000

    bug fix and lerp vertex

--- Assets/MarchingCubeGenerator.cs
+++ Assets/MarchingCubeGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,7 @@ public class MarchingCubeGenerator : MonoBehaviour
     [SerializeField]private int gridSizeZ = 15;
     [SerializeField] private float noiseScale;
     private Vector3 sphereCenter;
+
     [SerializeField]private float radius = 5;
     public bool visualizing;
     public float isolevel;
@@ -22,6 +24,7 @@ public class MarchingCubeGenerator : MonoBehaviour
     void Start()
     {
         meshFilter = GetComponent<MeshFilter>();
+        
         sphereCenter = new Vector3(gridSizeX/2,gridSizeY/2,gridSizeZ/2);
         scalarField = new float[gridSizeX, gridSizeY, gridSizeZ];
 
@@ -31,6 +34,7 @@ public class MarchingCubeGenerator : MonoBehaviour
                 for (int z = 0; z < gridSizeZ; z++) {
                 float distance = Vector3.Distance(new Vector3(x, y, z), sphereCenter);
                 scalarField[x, y, z] = (radius - distance);
+                //scalarField[x, y, z] = radius - Mathf.Sqrt((x - sphereCenter.x) * (x - sphereCenter.x) + (y - sphereCenter.y) * (y - sphereCenter.y) + (z - sphereCenter.z) * (z - sphereCenter.z));
                 }
             }
         }
@@ -39,22 +43,30 @@ public class MarchingCubeGenerator : MonoBehaviour
     }
 
     void MarchingCubes() {
-
         vertices.Clear();
         triangles.Clear();
-    for (int x = 0; x < gridSizeX - 1; x++) {
-        for (int y = 0; y < gridSizeY - 1; y++) {
-            for (int z = 0; z < gridSizeZ - 1; z++) {
+        for (int x = 0; x < gridSizeX - 1; x++) {
+            for (int y = 0; y < gridSizeY - 1; y++) {
+                for (int z = 0; z < gridSizeZ - 1; z++) {
                 ProcessCube(x, y, z);
+                }
             }
         }
     }
-}
 
 void ProcessCube(int x, int y, int z) {
     int cubeIndex = 0;
+    float[] cubeValues = new float[] {
+		scalarField[x, y, z + 1],
+		scalarField[x + 1, y, z + 1],
+		scalarField[x + 1, y, z],
+		scalarField[x, y, z],
+		scalarField[x, y + 1, z + 1],
+		scalarField[x + 1, y + 1, z + 1],
+		scalarField[x + 1, y + 1, z],
+		scalarField[x, y + 1, z]
+	};
 
-    // Determine the cube configuration
     for (int i = 0; i < 8; i++) {
         if (scalarField[x + MarchingCubesTables.vertexOffsets[i].x, y + MarchingCubesTables.vertexOffsets[i].y, z + MarchingCubesTables.vertexOffsets[i].z] < isolevel)
             cubeIndex |= 1 << i;
@@ -62,60 +74,47 @@ void ProcessCube(int x, int y, int z) {
 
     // Use edgeTable to find intersecting edges
     int[] edges = MarchingCubesTables.triTable[cubeIndex];
+    Vector3 worldPos = new Vector3(x, y, z);
+    for (int i = 0; edges[i] != -1; i += 3) {
+        int v00 = MarchingCubesTables.edgeConnections[edges[i]][0];
+		int v01 = MarchingCubesTables.edgeConnections[edges[i]][1];
 
-    for (int i = 0; i < 12; i++) {
-        if ((edges[i]) == -1) {
-            return;
-        }
-        else{
-            Vector3Int edgeStart = new Vector3Int(x,y,z) + MarchingCubesTables.Edges[edges[i],0];
-            Vector3Int edgeEnd = new Vector3Int(x,y,z) + MarchingCubesTables.Edges[edges[i],1];
-            Vector3 vertex = (edgeStart+edgeEnd)/2;
-            // Vector3 vertex = CulculateVertexPosition(edgeStart,scalarField[edgeStart.x,edgeStart.y,edgeStart.z],
-            // edgeEnd,scalarField[edgeEnd.x,edgeEnd.y,edgeEnd.z]
-            // ); // if interpolation, change this
-            
-            
-            vertices.Add(vertex);
-            triangles.Add(vertices.Count - 1);
-        }
-        
+        Vector3 v1 = CulculateVertexPosition(MarchingCubesTables.vertexOffsets[v00],cubeValues[v00],MarchingCubesTables.vertexOffsets[v01],cubeValues[v01])+worldPos;
+
+
+		int v10 = MarchingCubesTables.edgeConnections[edges[i + 1]][0];
+		int v11 = MarchingCubesTables.edgeConnections[edges[i + 1]][1];
+        Vector3 v2 = CulculateVertexPosition(MarchingCubesTables.vertexOffsets[v10],cubeValues[v10],MarchingCubesTables.vertexOffsets[v11],cubeValues[v11])+worldPos;
+
+		int v20 = MarchingCubesTables.edgeConnections[edges[i + 2]][0];
+		int v21 = MarchingCubesTables.edgeConnections[edges[i + 2]][1];
+        Vector3 v3 = CulculateVertexPosition(MarchingCubesTables.vertexOffsets[v20],cubeValues[v20],MarchingCubesTables.vertexOffsets[v21],cubeValues[v21])+worldPos;
+
+        AddTriangle(v1,v2,v3);
     }
-
-
-    // // Create triangles
-    // for (int i = 0; MarchingCubesTables.triTable[cubeIndex][i] != -1; i += 3) {
-    //     AddTriangle(edgeVertices[triangleTable[cubeIndex][i]],
-    //                 edgeVertices[triangleTable[cubeIndex][i + 1]],
-    //                 edgeVertices[triangleTable[cubeIndex][i + 2]]);
-    // }
 }
+    void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3) {
+        int triIndex = triangles.Count;
+        vertices.Add(v1);
+        vertices.Add(v2);
+        vertices.Add(v3);
 
-
-
-private Vector3 CulculateVertexPosition(Vector3 vertex1, float value1, Vector3 vertex2, float value2){
-    if (Mathf.Abs(isolevel-value1) < 0.0001){
-        return(vertex1);
+        triangles.Add(triIndex);
+        triangles.Add(triIndex + 1);
+        triangles.Add(triIndex + 2);
     }
-      
-    if (Mathf.Abs(isolevel-value2) < 0.0001){
-        return(vertex2);
-    }
-      
-    if (Mathf.Abs(value1-value2) < 0.0001){
-        return(vertex1);
-    }
-    return (vertex1 + (isolevel - value1) * (vertex2 - vertex1)  / (value2 - value1));
-}
-private void SetMesh()
-    {
-        Mesh mesh = new Mesh();
 
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = triangles.ToArray();
-        mesh.RecalculateNormals();
-
-        meshFilter.mesh = mesh;
+    private void SetMesh(){
+        if (vertices.Count > 0 && triangles.Count % 3 == 0) {
+            Mesh mesh = new Mesh();
+            mesh.SetVertices(vertices);
+            mesh.SetTriangles(triangles,0);
+            mesh.RecalculateNormals();
+            meshFilter.mesh = mesh;
+        } 
+        else {
+            Debug.LogWarning("Mesh generation failed: Invalid vertices or triangles.");
+        }
     }
 
 
@@ -125,13 +124,30 @@ private void SetMesh()
             return;
         }
         for (int x = 0; x < gridSizeX; x++) {
-        for (int y = 0; y < gridSizeY; y++) {
-            for (int z = 0; z < gridSizeZ; z++) {
-                Gizmos.color = new Color(scalarField[x,y,z],scalarField[x,y,z],scalarField[x,y,z],1);
-                Gizmos.DrawSphere(new Vector3(x,y,z),0.2f);
-        }
+            for (int y = 0; y < gridSizeY; y++) {
+                for (int z = 0; z < gridSizeZ; z++) {
+                    Gizmos.color = new Color(scalarField[x,y,z],scalarField[x,y,z],scalarField[x,y,z],1);
+                    Gizmos.DrawSphere(new Vector3(x,y,z),0.2f);
+                }
+            }
+        } 
     }
-} 
+
+
+
+    private Vector3 CulculateVertexPosition(Vector3 vertex1, float value1, Vector3 vertex2, float value2){
+        if (Mathf.Abs(isolevel-value1) < 0.0001){
+            return(vertex1);
+        }
+      
+        if (Mathf.Abs(isolevel-value2) < 0.0001){
+            return(vertex2);
+        }
+      
+        if (Mathf.Abs(value1-value2) < 0.0001){
+            return(vertex1);
+        }
+        return (vertex1 + (isolevel - value1) * (vertex2 - vertex1)  / (value2 - value1));
     }
 
 }
